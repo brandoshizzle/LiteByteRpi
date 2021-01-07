d40363cfe733f8f6ba1ffb2dc7be258dad7b1af8
brandoshizzle <cathcart.brandon@gmail.com>
2021-01-06 23:17:53 -0700

    back to 2 mins

--- litebyte.js
+++ litebyte.js
@@ -126,7 +126,7 @@ class Example {
 
 		// console.log(this.gallery);
 		// Every 2 minute
-		setInterval(this.loop.bind(this), 1000);
+		setInterval(this.loop.bind(this), 1000 * 60 * 2);
 		// for (var x = 0; x < this.config.width; x++) {
 		// 	for (var y = 0; y < this.config.height; y++) {
 		// 		const row = y.toString();
